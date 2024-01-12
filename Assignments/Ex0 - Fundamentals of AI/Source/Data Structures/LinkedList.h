8e71d02b97488e3a5dc680adc49a798454065e0f
tybeller <ty.c.beller@gmail.com>
2024-01-12 11:51:47 -0500

    rewrite debug

--- Assignments/Ex0 - Fundamentals of AI/Source/Data Structures/LinkedList.h
+++ Assignments/Ex0 - Fundamentals of AI/Source/Data Structures/LinkedList.h
@@ -9,12 +9,13 @@ namespace ufl_cap4053 { namespace fundamentals {
 		Node<T>* prev;
 		Node<T>* next;
 	public:
-		Node<T>(data) {
+		Node<T>(T data) {
 			this->data = data;
 			prev = nullptr;
 			next = nullptr;
 		}
-		friend class LinkedList<T>;
+		template <class T>
+		friend class LinkedList;
 	};
 	
 
@@ -23,17 +24,105 @@ namespace ufl_cap4053 { namespace fundamentals {
 	private:
 		Node<T>* front;
 		Node<T>* back;
-
 	public:
-
 		LinkedList<T>() {
 			front = nullptr;
 			back = nullptr;
 		}
+		Iterator begin() const {
+			return Iterator(front);
+		}
+		Iterator end() const {
+			return Iterator(nullptr);
+		}
+		bool isEmpty() const {
+			return front == nullptr;
+		}
+		T getFront() const {
+			return front;
+		}
+		T getBack() const {
+			return back;
+		}
+		void enqueue(T element) {
+			Node<T>* newNode = new Node<T>(element);
+			back->next = newNode;
+			newNode->prev = back;
+			back = newNode;
+		}
+		void dequeue() {
+			Node<T>* newFront = front->next;
+			delete front;
+			newFront->prev = nullptr;
+			front = newFront;
+		}
+		void pop() {
+			Node<T>* newBack = back->prev;
+			delete back;
+			newBack->next = nullptr;
+			back = newBack;
+		}
+		void clear() {
+			Node<T>* curr = front;
+			while (curr != nullptr) {
+				Node<T>* temp = curr;
+				curr = curr->next;
+				delete temp;
+			}
+			front = nullptr;
+			back = nullptr;
+		}
+		bool contains(T element) const {
+			Node<T>* curr = front;
+			while (curr != nullptr) {
+				if (curr->data == element) {
+					return true;
+				}
+				curr = curr->next;
+			}
+			return false;
+		}
+		void remove(T element) {
+			Node<T>* curr = front;
+			while (curr != nullptr) {
+				if (curr->data == element) {
+					Node<T>* nodePrev = curr->prev;
+					Node<T>* nodeNext = curr->next;
+
+					delete curr;
+
+					nodePrev->next = nodeNext;
+					nodeNext->prev = nodePrev;
+				}
+				curr = curr->next;
+			}
+			return;
+		}
 
 		class Iterator {
+		private:
+			Node<T>* curr;
+
+		public:
+			Iterator(Node<T>* node) : curr(node) {}
+
+			T operator*() const {
+				return curr->data;
+			}
+			Iterator& operator++() {
+				curr = curr->next;
+				return curr;
+			}
+			bool operator==(Iterator const& rhs) {
+				return curr == rhs.curr;
+			}
+			bool operator!=(Iterator const& rhs) {
+				return !(curr == rhs.curr);
+			}
 
 		};
+
+		friend class Iterator;
 	};
 
 	
